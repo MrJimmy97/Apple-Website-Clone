b2fe578f68785ee759eb84f91e778c9236224891
Jimmy <jimmy973878@gmail.com>
2022-09-19 03:28:28 +0800

    Create  `dictionaryData` for `AboutApple` use

--- src/Components/AboutApple/AboutApple.js
+++ src/Components/AboutApple/AboutApple.js
@@ -1,6 +1,7 @@
 import styled, { css } from "styled-components/macro";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAppleWhole } from "@fortawesome/free-solid-svg-icons";
+import directoryData from "../dictionaryData";
 
 const Section = styled.div`
   width: 100%;
---
+++ src/Components/dictionaryData.js
@@ -0,0 +1,103 @@
+const directoryData = [
+    {
+      title: "Shop and Learn",
+      directoryLink: [
+        { title: 'Store', key: "z2" },
+        { title: 'Mac', key: "z3" },
+        { title: 'iPad', key: "z4" },
+        { title: 'iPhone', key: "z5" },
+        { title: 'Watch', key: "z6" },
+        { title: 'AirPods', key: "z7" },
+        { title: 'TV & Home', key: "z8" },
+        { title: 'AirTag', key: "z9" },
+        { title: 'Accessories', key: "z10" },
+        { title: 'Gift Cards', key: "z11" },
+      ],
+      key: "z1",
+    },
+    {
+      title: "Services",
+      directoryLink: [
+        { title: 'Apple Music', key: "y2" },
+        { title: 'Apple TV+', key: "y3" },
+        { title: 'iCloud', key: "y4" },
+        { title: 'Apple Pay', key: "y5" },
+        { title: 'Apple Books', key: "y6" },
+        { title: 'Apple Podscasts', key: "y7" },
+        { title: 'Apple Store', key: "y8" },
+      ],
+      key: "y1",
+    },
+    {
+      title: "Account",
+      directoryLink: [
+        { title: 'Manage Your Apple ID', key: "x2" },
+        { title: 'Apple Store Account', key: "x3" },
+        { title: 'iCloud.com', key: "x4" },
+      ],
+      key: "x1",
+    },
+    {
+      title: "Apple Store",
+      directoryLink: [
+        { title: 'Find a Store', key: "w2" },
+        { title: 'Genius Bar', key: "w3" },
+        { title: 'Today at Apple', key: "w4" },
+        { title: 'Apple Camp', key: "w5" },
+        { title: 'Apple Store App', key: "w6" },
+        { title: 'Refurbished and Clearance', key: "w7" },
+        { title: 'Apple Trade in', key: "w8" },
+        { title: 'Order Status', key: "w9" },
+        { title: 'Shopping Help', key: "w10" },
+      ],
+      key: "w1",
+    },
+    {
+      title: "For Business",
+      directoryLink: [
+        { title: 'Apple and Business', key: "w2" },
+        { title: 'Shop for Business', key: "w3" },
+      ],
+      key: "v1",
+    },
+    {
+      title: "For Education",
+      directoryLink: [
+        { title: 'Apple and Education', key: "u2" },
+        { title: 'Shop for College', key: "u3" },
+      ],
+      key: "u1",
+    },
+    {
+      title: "For Healthcare",
+      directoryLink: [
+        { title: 'Apple in Healthcare', key: "t2" },
+        { title: 'Health on Apple Watch', key: "t3" },
+      ],
+      key: "t1",
+    },
+    {
+      title: "Apple Values",
+      directoryLink: [
+        { title: 'Accessibility', key: "s2" },
+        { title: 'Environment', key: "s3" },
+        { title: 'Privacy', key: "s4" },
+        { title: 'Supplier Responsibility', key: "s5" },
+      ],
+      key: "s1",
+    },
+    {
+      title: "About Apple",
+      directoryLink: [
+        { title: 'Newsroom', key: "r2" },
+        { title: 'Apple Leadership', key: "r3" },
+        { title: 'Career Opportunities', key: "r4" },
+        { title: 'Ethics & Compliance', key: "r5" },
+        { title: 'Events', key: "r6" },
+        { title: 'Contact Apple', key: "r7" },
+      ],
+      key: "r1",
+    },
+  ];
+
+  export default directoryData
